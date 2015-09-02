3b1d87a921149de10756daf7da29fa30f0036fbd
okadaa <notoilet.nolife@gmail.com>
2015-09-02 20:06:28 +0900

    origin

---
+++ basicninsyo.rb
@@ -0,0 +1,15 @@
+# app/controllers/application_controller.rb
+
+class ApplicationController < ActionController::Base
+
+  before_action :basic_authentication if Rails.env.production?
+
+  private
+
+  def basic_authentication
+    authenticate_or_request_with_http_basic do |user, password|
+      user == [ユーザー名] && password == [パスワード]
+    end
+  end
+
+end
